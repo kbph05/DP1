8c0b131a18ec2da0774a1f0a66e101bf0333f674
Kane <kzhan@student.ubc.ca>
2026-02-13 17:13:01 -0800

    Add testbenches, renaming

---
+++ SourceCode/CSAN.vhd
@@ -0,0 +1,67 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+ENTITY CSAN IS
+GENERIC (N: NATURAL := 64;
+			C: NATURAL := 4);
+PORT (X: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+		Y: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+		S: OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+		Cin: IN STD_LOGIC;
+		Cout: OUT STD_LOGIC);
+END CSAN;
+
+ARCHITECTURE LogicFuncCSAN OF CSAN IS
+-- signal decs
+SIGNAL CwC0, CwC1: STD_LOGIC;
+SIGNAL SwC0, SwC1, SR: STD_LOGIC_VECTOR((N-1)/2 DOWNTO 0);
+SIGNAL Int0, Int1: STD_LOGIC_VECTOR((N+1)/2 DOWNTO 0);
+SIGNAL MuxResult: STD_LOGIC_VECTOR((N+1)/2 DOWNTO 0);
+SIGNAL tempC: STD_LOGIC;
+BEGIN
+gen: IF N > C GENERATE
+	left_half_upper: 
+		ENTITY work.CSAN(LogicFuncCSAN)
+		GENERIC MAP (N => N/2)
+		PORT MAP(X=>X(N-1 DOWNTO (N+1)/2), Y=>Y(N-1 DOWNTO (N+1)/2), S=>SwC0, Cin => '0', Cout => CwC0);
+
+	left_half_lower: 
+		ENTITY work.CSAN(LogicFuncCSAN)
+		GENERIC MAP (N => N/2)
+		PORT MAP(X=>X(N-1 DOWNTO (N+1)/2), Y=>Y(N-1 DOWNTO (N+1)/2), S=>SwC1, Cin => '1', Cout => CwC1);
+	
+	Int0 <= CWC0 & SwC0;
+	Int1 <= CWC1 & SwC1;
+
+	mux:
+		ENTITY work.Mux2cNb 
+		GENERIC MAP(N => N/2 + 1, C => 1)
+		PORT MAP(x1 =>	Int0, x2 => Int1, s => tempC, y => MuxResult);
+		Cout <= MuxResult((N+1)/2);
+		S(N-1 DOWNTO (N+1)/2) <= MuxResult((N-1)/2 DOWNTO 0);
+
+	right_half: 
+		ENTITY work.CSAN(LogicFuncCSAN)
+		GENERIC MAP (N => N/2)
+		PORT MAP(
+		X=>X((N-1)/2 DOWNTO 0), 
+		Y=>Y((N-1)/2 DOWNTO 0), 
+		S=>SR, 
+		Cin => Cin, 
+		Cout => tempC
+		);		
+		S((N-1)/2 DOWNTO 0) <= SR;
+END GENERATE gen;
+
+
+genbase: IF N <= C GENERATE
+	-- Use base full adder
+	base_case_fa:
+		ENTITY work.FA(LogicFuncFA) 
+		GENERIC MAP (C => N)
+		PORT MAP(X=>X(C-1 DOWNTO 0), Y=>Y(C-1 DOWNTO 0), S=>S(C-1 DOWNTO 0), Cin => Cin, Cout => Cout);
+END GENERATE genbase;
+
+
+
+
+END LogicFuncCSAN;
---
+++ SourceCode/RCAN.vhd
@@ -0,0 +1,49 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- WORK SUMMARY (to add to project log excel):
+-- 2/13:	12-2:00 pm - fixed the carries of fastripple, fixed issue with the types not matching with baseline
+-- 2/11: 2-8:00 pm - wrote a new version of RCAN; using now a seperate entity for a 1 bit adder rather than the rtl showing a bunch of gates, this way
+--							will contain each adder in a box - a lot cleaner
+-- 2/10: 3-5:00 pm - wrote first version of RCAN and fastripple architecture - one entity, one architecture, all in one file 
+
+-- Take the FullAddr entity to make 64 full adders into a ripple adder
+Entity RCAN is
+generic( 
+	N : natural := 64
+);
+port (
+		X, Y 	: in std_logic_vector(N-1 downto 0);
+		Cin	: in std_logic;
+		S		: out std_logic_vector(N-1 downto 0);
+		Cout 	: out std_logic
+);
+end RCAN;
+
+-- FastRipple Adder
+architecture FastRipple of RCAN is
+
+signal carries : std_logic_vector(N-1 downto 0);
+
+begin
+--		S[0]:
+		U0 : entity work.FullAddr(behavioural) port map(X => X(0), Y => Y(0), Cin => Cin, S => S(0), Cout => carries(0));
+--		S[62:1]:
+		gen0 : for i in 1 to N-2 generate
+			U1 : entity work.FullAddr(behavioural) port map(X => X(i), Y => Y(i), Cin => carries(i-1), S => S(i), Cout => carries(i));
+		end generate gen0;
+--		S[63:
+		U3 : entity work.FullAddr(behavioural) port map(X => X(N-1), Y => Y(N-1), Cin => carries(N-2), S => S(N-1), Cout => Cout);
+end architecture;
+
+-- Synthesized ripple adder:
+architecture Baseline of RCAN is
+ begin
+	process(X,Y)
+	begin
+		for i in 1 to N-1 loop 
+			S <= std_logic_vector(signed(X) + signed(Y));
+		end loop;
+	end process;
+end architecture;
